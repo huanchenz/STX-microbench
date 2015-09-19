e48f3025010ec670404c5b5a232d6c5d12cfd67a
conglongli <ubuntu@node1.maas>
2015-09-19 18:46:44 +0000

    update scripts

--- run_stxbtree_compact.sh
+++ run_stxbtree_compact.sh
@@ -1,53 +1,23 @@
 ./stxbtree_compact_random_int_a
-#./stxbtree_compact_random_int_a
-#./stxbtree_compact_random_int_a
 
 ./stxbtree_compact_random_int_c
-#./stxbtree_compact_random_int_c
-#./stxbtree_compact_random_int_c
 
 ./stxbtree_compact_random_int_e
-#./stxbtree_compact_random_int_e
-#./stxbtree_compact_random_int_e
 
 ./stxbtree_compact_monoinc_int_a
-#./stxbtree_compact_monoinc_int_a
-#./stxbtree_compact_monoinc_int_a
 
 ./stxbtree_compact_monoinc_int_c
-#./stxbtree_compact_monoinc_int_c
-#./stxbtree_compact_monoinc_int_c
 
 ./stxbtree_compact_monoinc_int_e
-#./stxbtree_compact_monoinc_int_e
-#./stxbtree_compact_monoinc_int_e
 
 ./stxbtree_compact_random_str_a
-#./stxbtree_compact_random_str_a
-#./stxbtree_compact_random_str_a
 
 ./stxbtree_compact_random_str_c
-#./stxbtree_compact_random_str_c
-#./stxbtree_compact_random_str_c
 
 ./stxbtree_compact_random_str_e
-#./stxbtree_compact_random_str_e
-#./stxbtree_compact_random_str_e
 
 ./stxbtree_compact_email_a
-#./stxbtree_compact_email_a
-#./stxbtree_compact_email_a
 
 ./stxbtree_compact_email_c
-#./stxbtree_compact_email_c
-#./stxbtree_compact_email_c
 
 ./stxbtree_compact_email_e
-#./stxbtree_compact_email_e
-#./stxbtree_compact_email_e
-
-#./stxbtree_compact_url_a
-
-#./stxbtree_compact_url_c
-
-#./stxbtree_compact_url_e
--- run_stxbtree_ori.sh
+++ run_stxbtree_ori.sh
@@ -1,59 +1,23 @@
 ./stxbtree_ori_random_int_a
-#./stxbtree_ori_random_int_a
-#./stxbtree_ori_random_int_a
 
 ./stxbtree_ori_random_int_c
-#./stxbtree_ori_random_int_c
-#./stxbtree_ori_random_int_c
 
 ./stxbtree_ori_random_int_e
-#./stxbtree_ori_random_int_e
-#./stxbtree_ori_random_int_e
 
 ./stxbtree_ori_monoinc_int_a
-#./stxbtree_ori_monoinc_int_a
-#./stxbtree_ori_monoinc_int_a
 
 ./stxbtree_ori_monoinc_int_c
-#./stxbtree_ori_monoinc_int_c
-#./stxbtree_ori_monoinc_int_c
 
 ./stxbtree_ori_monoinc_int_e
-#./stxbtree_ori_monoinc_int_e
-#./stxbtree_ori_monoinc_int_e
 
 ./stxbtree_ori_random_str_a
-#./stxbtree_ori_random_str_a
-#./stxbtree_ori_random_str_a
 
 ./stxbtree_ori_random_str_c
-#./stxbtree_ori_random_str_c
-#./stxbtree_ori_random_str_c
 
 ./stxbtree_ori_random_str_e
-#./stxbtree_ori_random_str_e
-#./stxbtree_ori_random_str_e
 
 ./stxbtree_ori_email_a
-#./stxbtree_ori_email_a
-#./stxbtree_ori_email_a
 
 ./stxbtree_ori_email_c
-#./stxbtree_ori_email_c
-#./stxbtree_ori_email_c
 
 ./stxbtree_ori_email_e
-#./stxbtree_ori_email_e
-#./stxbtree_ori_email_e
-
-#./stxbtree_ori_url_a
-#./stxbtree_ori_url_a
-#./stxbtree_ori_url_a
-
-#./stxbtree_ori_url_c
-#./stxbtree_ori_url_c
-#./stxbtree_ori_url_c
-
-#./stxbtree_ori_url_e
-#./stxbtree_ori_url_e
-#./stxbtree_ori_url_e
