f3cbc998266e97114dd2765fcfd2ca17760a24d8
conglongli <ubuntu@node1.maas>
2015-09-21 21:23:29 +0000

    updated scripts

--- run_stxbtree_multi_ori.sh
+++ run_stxbtree_multi_ori.sh
@@ -1,23 +1,45 @@
-./stxbtree_multi_ori_random_int_a
+#!bin/bash
 
-./stxbtree_multi_ori_random_int_c
+OUTPUT_FILE=./throughputs/stxbtree_multi_ori_256.txt
 
-./stxbtree_multi_ori_random_int_e
+echo "./stxbtree_multi_ori_random_int_a"
+./stxbtree_multi_ori_random_int_a > $OUTPUT_FILE
 
-./stxbtree_multi_ori_monoinc_int_a
+echo "./stxbtree_multi_ori_random_int_c"
+./stxbtree_multi_ori_random_int_c >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_monoinc_int_c
+echo "./stxbtree_multi_ori_random_int_e"
+./stxbtree_multi_ori_random_int_e >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_monoinc_int_e
+echo >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_random_str_a
+echo "./stxbtree_multi_ori_monoinc_int_a"
+./stxbtree_multi_ori_monoinc_int_a >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_random_str_c
+echo "./stxbtree_multi_ori_monoinc_int_c"
+./stxbtree_multi_ori_monoinc_int_c >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_random_str_e
+echo "./stxbtree_multi_ori_monoinc_int_e"
+./stxbtree_multi_ori_monoinc_int_e >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_email_a
+echo >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_email_c
+echo "./stxbtree_multi_ori_random_str_a"
+./stxbtree_multi_ori_random_str_a >> $OUTPUT_FILE
 
-./stxbtree_multi_ori_email_e
+echo "./stxbtree_multi_ori_random_str_c"
+./stxbtree_multi_ori_random_str_c >> $OUTPUT_FILE
+
+echo "./stxbtree_multi_ori_random_str_e"
+./stxbtree_multi_ori_random_str_e >> $OUTPUT_FILE
+
+echo >> $OUTPUT_FILE
+
+echo "./stxbtree_multi_ori_email_a"
+./stxbtree_multi_ori_email_a >> $OUTPUT_FILE
+
+echo "./stxbtree_multi_ori_email_c"
+./stxbtree_multi_ori_email_c >> $OUTPUT_FILE
+
+echo "./stxbtree_multi_ori_email_e"
+./stxbtree_multi_ori_email_e >> $OUTPUT_FILE
